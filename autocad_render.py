0ea499b518b1f7bda2c87e12e1518fff18229d36
wyttnytt <105594685+wyttnytt@users.noreply.github.com>
2024-04-18 22:39:40 +0800

    Update autocad_render.py

--- autocad_render.py
+++ autocad_render.py
@@ -1,56 +1,71 @@
 from pyautocad import Autocad, APoint, aDouble
 
 acad = Autocad(create_if_not_exists=True)
+cubes={}
 
 class Cube():
+
     def __init__(self, width, height) :
         self.width = width
         self.height = height
         self.vertices = 4
     
-    def calculate_xy(self, xy):
-        lxy = (map(float, xy.split(", ")))
-        tlxy = tuple(lxy)
-        return tlxy
-    
-    def calculate_exy(self, tlxy):
+
+    def calculate_distance(self):
+        global cubes
+        x, y = 0.0, 0.0
+        if len(cubes)!=0:
+            for cube in cubes:
+                width = cubes[cube].width
+                x = x+width
+                y = y
+        self.tlxy = (x, y)
+
+    def calculate_exy(self):
         exy=[]
         for vertice in range(0, self.vertices*2):                                             
             if vertice==0:
-                x=tlxy[0]
-                y=tlxy[1]
+                x=self.tlxy[0]
+                y=self.tlxy[1]
                 z=0
             elif vertice==1:
-                x=tlxy[0]
-                y=tlxy[1]+self.height
+                x=self.tlxy[0]
+                y=self.tlxy[1]+self.height
                 z=0
             elif vertice==2:
-                x=tlxy[0]+self.width
-                y=tlxy[1]+self.height
+                x=self.tlxy[0]+self.width
+                y=self.tlxy[1]+self.height
                 z=0
             elif vertice==3:
-                x=tlxy[0]+self.width
-                y=tlxy[1]
+                x=self.tlxy[0]+self.width
+                y=self.tlxy[1]
                 z=0
             
             self.crd = [x, y, z]
     
             exy.extend(self.crd)
-        fp = [tlxy[0], tlxy[1], 0]
+        fp = [self.tlxy[0], self.tlxy[1], 0]
         exy.extend(fp)
-        print(exy)
+
         return exy
     
     def texy(self, exy):
         texy=tuple(exy)
         return texy
     
+def add_cube(width:float, height:float):
+    global cubes
+    cube=Cube(width, height)
+    cube.calculate_distance()
+    exy=cube.calculate_exy()
+    texy=cube.texy(exy)
+    polygon = aDouble(texy)
+    polygond = acad.model.AddPolyline(polygon)
+    cubes["cube_"+str(len(cubes)+1)]=cube
 
 
-cube=Cube(12.5, 12.5)
-tlxy=cube.calculate_xy("0, 0")
-exy=cube.calculate_exy(tlxy)
-texy=cube.texy(exy)
+polygon_1 = add_cube(12.5, 12.5)
 
-polygon = aDouble(texy)
-polygond = acad.model.AddPolyline(polygon)
+polygon_2 = add_cube(12.5, 12.5)
+
+polygon_3 = add_cube(12.5, 12.5)
